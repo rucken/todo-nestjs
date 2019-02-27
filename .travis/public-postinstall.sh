790a1a11982952c066ada486d106cbc89abe9b4f
EndyKaufman <admin@site15.ru>
2019-02-27 18:17:45 +0500

    chore: Update deploy scripts

--- .travis/public-postinstall.sh
+++ .travis/public-postinstall.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 node ./scripts/patch.js
-npm i --save ./dist/rucken/todo-nestjs
-mkdir -p ./vendors/rucken/core-nestjs
-mkdir -p ./vendors/rucken/auth-nestjs
-cp -rf ./node_modules/@rucken/core-nestjs/* ./vendors/rucken/core-nestjs
-cp -rf ./node_modules/@rucken/auth-nestjs/* ./vendors/rucken/auth-nestjs
-npm link ./vendors/rucken/core-nestjs
-npm link ./vendors/rucken/auth-nestjs
