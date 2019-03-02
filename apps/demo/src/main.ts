f961920b46ddfd5f0afc76339eee02747a36ac9a
EndyKaufman <admin@site15.ru>
2019-03-02 14:08:17 +0500

    chore: Remove update chmod for sqlite db file

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,7 +1,5 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { chmodSync } from 'fs';
 import 'reflect-metadata';
 import { register } from 'tsconfig-paths';
 import { AppModule } from './app/app.module';
@@ -21,16 +19,6 @@ if (config.env.name !== 'develop') {
   });
 }
 async function bootstrap() {
-  /**
-   * Set writeble db file if is set
-   */
-  if (config.db.file) {
-    try {
-      chmodSync(config.db.file, 777);
-    } catch (error) {
-      Logger.log(`error on set chmod 777 to database file ${config.db.file}`, 'Main');
-    }
-  }
 
   /**
    * Create nest application
