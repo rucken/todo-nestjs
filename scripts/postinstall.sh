977bccffa2ecded0ee079bb3c5ca076f245f7dc8
EndyKaufman <admin@site15.ru>
2019-04-14 15:32:41 +0500

    chore: Remove node_modules from vendors

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,8 +4,8 @@ mkdir -p ./vendors/rucken/core-nestjs
 mkdir -p ./vendors/rucken/auth-nestjs
 cp -rf ./node_modules/@rucken/core-nestjs/* ./vendors/rucken/core-nestjs
 cp -rf ./node_modules/@rucken/auth-nestjs/* ./vendors/rucken/auth-nestjs
-rm -rf ./vendors/rucken/core-nestjs/node_modules
-rm -rf ./vendors/rucken/auth-nestjs/node_modules
 npm link ./vendors/rucken/core-nestjs
 npm link ./vendors/rucken/auth-nestjs
 fi
+rm -rf ./vendors/rucken/core-nestjs/node_modules
+rm -rf ./vendors/rucken/auth-nestjs/node_modules
