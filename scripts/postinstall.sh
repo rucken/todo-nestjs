664ac00b4f31c70d54092e82fc5d76740067f1e2
EndyKaufman <admin@site15.ru>
2019-04-24 10:56:01 +0500

    chore: Update postinstall script No. 6

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 node ./scripts/patch.js
-if [ ! -f "./vendors/rucken/core-nestjs/package.json" ]; then
+if [ ! -d "./vendors/rucken/core-nestjs" ]; then
 mkdir -p ./vendors/rucken/core-nestjs
 fi
 if [ -f "./node_modules/@rucken/core-nestjs/package.json" ]; then
@@ -8,10 +8,9 @@ fi
 if [ -f "./vendors/rucken/core-nestjs/package.json" ]; then
 npm link ./vendors/rucken/core-nestjs
 fi
-if [ -f "./vendors/rucken/core-nestjs/node_modules" ]; then
+if [ -d "./vendors/rucken/core-nestjs/node_modules" ]; then
 rm -rf ./vendors/rucken/core-nestjs/node_modules
 fi
-
 if [ ! -f "./vendors/rucken/auth-nestjs/package.json" ] ; then
 mkdir -p ./vendors/rucken/auth-nestjs
 fi
@@ -21,6 +20,6 @@ fi
 if [ -f "./vendors/rucken/auth-nestjs/package.json" ]; then
 npm link ./vendors/rucken/auth-nestjs
 fi
-if [ -f "./vendors/rucken/auth-nestjs/node_modules" ]; then
+if [ -d "./vendors/rucken/auth-nestjs/node_modules" ]; then
 rm -rf ./vendors/rucken/auth-nestjs/node_modules
 fi
