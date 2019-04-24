0c66ec8ba85f30d1de699d25742c1dac0603ca39
EndyKaufman <admin@site15.ru>
2019-04-24 09:29:37 +0500

    chore: Update postinstall script No. 5

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,26 +1,26 @@
 node ./scripts/patch.js
-if [ ! -d "./vendors/rucken/core-nestjs" ]; then
+if [ ! -f "./vendors/rucken/core-nestjs/package.json" ]; then
 mkdir -p ./vendors/rucken/core-nestjs
 fi
-if [ -d "./node_modules/@rucken/core-nestjs" ]; then
+if [ -f "./node_modules/@rucken/core-nestjs/package.json" ]; then
 cp -rf ./node_modules/@rucken/core-nestjs/* ./vendors/rucken/core-nestjs
 fi
-if [ -d "./vendors/rucken/core-nestjs" ]; then
+if [ -f "./vendors/rucken/core-nestjs/package.json" ]; then
 npm link ./vendors/rucken/core-nestjs
 fi
-if [ -d "./vendors/rucken/core-nestjs/node_modules" ]; then
+if [ -f "./vendors/rucken/core-nestjs/node_modules" ]; then
 rm -rf ./vendors/rucken/core-nestjs/node_modules
 fi
 
-if [ ! -d "./vendors/rucken/auth-nestjs" ] ; then
+if [ ! -f "./vendors/rucken/auth-nestjs/package.json" ] ; then
 mkdir -p ./vendors/rucken/auth-nestjs
 fi
-if [ -d "./node_modules/@rucken/auth-nestjs" ]; then
+if [ -f "./node_modules/@rucken/auth-nestjs/package.json" ]; then
 cp -rf ./node_modules/@rucken/auth-nestjs/* ./vendors/rucken/auth-nestjs
 fi
-if [ -d "./vendors/rucken/auth-nestjs" ]; then
+if [ -f "./vendors/rucken/auth-nestjs/package.json" ]; then
 npm link ./vendors/rucken/auth-nestjs
 fi
-if [ -d "./vendors/rucken/auth-nestjs/node_modules" ]; then
+if [ -f "./vendors/rucken/auth-nestjs/node_modules" ]; then
 rm -rf ./vendors/rucken/auth-nestjs/node_modules
 fi
