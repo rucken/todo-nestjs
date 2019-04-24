c85d5ace94ba6f594157638561bc822a70e056f4
EndyKaufman <admin@site15.ru>
2019-04-24 08:40:39 +0500

    chore: Update postinstall script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,5 @@
 node ./scripts/patch.js
-if [ ! -d "./vendors/rucken/core-nestjs" ] && [ ! -d "./node_modules/@rucken/core-nestjs" ]; then
+if [ ! -d "./vendors/rucken/core-nestjs" ] && [ -d "./node_modules/@rucken/core-nestjs" ]; then
 mkdir -p ./vendors/rucken/core-nestjs
 cp -rf ./node_modules/@rucken/core-nestjs/* ./vendors/rucken/core-nestjs
 npm link ./vendors/rucken/core-nestjs
@@ -8,7 +8,7 @@ if [ -d "./vendors/rucken/core-nestjs/node_modules" ]; then
 rm -rf ./vendors/rucken/core-nestjs/node_modules
 fi
 
-if [ ! -d "./vendors/rucken/auth-nestjs" ] && [ ! -d "./node_modules/@rucken/auth-nestjs" ]; then
+if [ ! -d "./vendors/rucken/auth-nestjs" ] && [ -d "./node_modules/@rucken/auth-nestjs" ]; then
 mkdir -p ./vendors/rucken/auth-nestjs
 cp -rf ./node_modules/@rucken/auth-nestjs/* ./vendors/rucken/auth-nestjs
 npm link ./vendors/rucken/auth-nestjs
